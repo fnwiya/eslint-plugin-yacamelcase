bf00e53fc185426b1cb9405cdbe865b79e4c7658
fumiya_kohno <fumiya_kohno@forcia.com>
2017-03-25 19:05:06 +0900

    initial commit

---
+++ tests/yacamelcase.js
@@ -0,0 +1,52 @@
+/**
+ * @fileoverview Yet another camelcase rule
+ * @author kohno
+ */
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var RuleTester = require("eslint").RuleTester;
+var rule = require("../rule/yacamelcase");
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+
+ruleTester.run("yacamelcase", rule, {
+	valid: [
+		{
+			code: "var should_ignored = \"hoge\"",
+			options: [{ ignoreStrings: ["should_ignored"]}]
+		},
+		{
+			code: "var should_ignored = \"hoge\"; var should_ignored_too = \"hoge\";",
+			options: [{ ignoreStrings: ["should_ignored", "should_ignored_too"]}]
+		}
+
+	],
+	invalid: [
+		{
+			code: "var should_error = \"hoge\"",
+			errors: [
+				{
+					message: "Identifier 'should_error' is not in camel case.",
+					type: "Identifier"
+				}
+			]
+		},
+		{
+			code: "var should_ignored = \"hoge\"; var should_error = \"hoge\"",
+			options: [{ ignoreStrings: ["should_ignored"]}],
+			errors: [
+				{
+					message: "Identifier 'should_error' is not in camel case.",
+					type: "Identifier"
+				}
+			]
+		}
+	]
+});
